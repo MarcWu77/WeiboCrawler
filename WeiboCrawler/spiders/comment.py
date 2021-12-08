64d307828ce195a2e73fde8739ce9dbf48526390
MarcWu77 <95768073+MarcWu77@users.noreply.github.com>
2021-12-09 01:42:00 +0800

    Update comment.py

--- WeiboCrawler/spiders/comment.py
+++ WeiboCrawler/spiders/comment.py
@@ -11,7 +11,7 @@ class CommentSpider(Spider):
     base_url = 'https://api.weibo.cn/2/comments/build_comments?'
 
     def start_requests(self):
-        mblog_ids = ['4704521537720325']
+        mblog_ids = ['2696049583','wuxijiaojing']
         urls = [f'{self.base_url}is_show_bulletin=2&c=android&s=746fd605&id={mblog_id}&from=10A8195010&gsid=_2AkMolNMzf8NhqwJRmf4dxWzgb49zzQrEieKeyCLoJRM3HRl-wT9jqmwMtRV6AgOZP3LqGBH-29qGRB4vP3j-Hng6DkBJ&count=50&max_id_type=1' for mblog_id in mblog_ids]
         for url in urls:
             yield Request(url, callback=self.parse, dont_filter=True, headers={'Host': 'api.weibo.cn'})
